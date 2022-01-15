bd46d90d270d2c628ab905d707dd830b65902c33
Neethu.V <neetz989@gmail.com>
2022-01-15 22:20:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const model=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnCloseModal=document.querySelector('.close-modal');
+const btnsOpenModal=document.querySelectorAll('.show-modal');
+
+
+function openModal(){
+    model.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+function closeModal(){
+    model.classList.add('hidden');
+    overlay.classList.add('hidden');
+
+};
+
+for(let i=0; i < btnsOpenModal.length;i++){
+    btnsOpenModal[i].addEventListener('click',openModal);
+}
+
+btnCloseModal.addEventListener('click',closeModal);
+
+overlay.addEventListener('click',closeModal);
